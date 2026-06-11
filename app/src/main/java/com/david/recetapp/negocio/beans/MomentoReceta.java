ea7b03e2e83656384c613e702382866e4e1c98d1
DAVIDL <davidlucasmora81@gmail.com>
2026-06-11 19:47:52 +0200

    Mejoras en la gestión de recetas: adición de opción para seleccionar el momento de la receta y actualización de la interfaz

---
+++ app/src/main/java/com/david/recetapp/negocio/beans/MomentoReceta.java
@@ -0,0 +1,19 @@
+package com.david.recetapp.negocio.beans;
+
+import com.david.recetapp.R;
+
+public enum MomentoReceta {
+    COMIDA(R.string.comida),
+    CENA(R.string.cena),
+    AMBOS(R.string.ambos);
+
+    private final int stringRes;
+
+    MomentoReceta(int stringRes) {
+        this.stringRes = stringRes;
+    }
+
+    public int getStringRes() {
+        return stringRes;
+    }
+}
